cfaa494df74c81270ab0e2cb9f11bc6a5a015340
Agustina Belén Fernández <agustinabelenfernandez05@gmail.com>
2024-11-06 20:05:29 -0300

    ADD: Agregando mostrar opciones del juego

--- funciones/mostrar_reglas.py
+++ funciones/mostrar_reglas.py
@@ -41,3 +41,17 @@ def mostrar_debilidades_de_tipos() -> None:
     """
     
     print(debilidades)
+
+
+def mostrar_menu_opciones() -> None:
+    """Muestra el menú de opciónes que hay dentro del juego  """
+    
+    opciones = """ 
+    
+    1. Volver a ver las reglas del juego
+    2. Ver debilidades de los tipos de Pokemones
+    3. Jugar
+    4. Salir del juego
+    """
+    
+    print(opciones)
