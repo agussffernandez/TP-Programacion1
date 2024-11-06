b02c40dc6e9aeebf1e0c1365aba1ecace7aa534a
Agustina Belén Fernández <agustinabelenfernandez05@gmail.com>
2024-11-06 20:00:21 -0300

    ADD: Agregando mostrar debilidades de tipos

--- funciones/mostrar_reglas.py
+++ funciones/mostrar_reglas.py
@@ -26,3 +26,18 @@ def mostrar_reglas() -> None :
     """
     
     print(reglas)
+
+
+def mostrar_debilidades_de_tipos() -> None:
+    """ Muestra las debilidades de cada tipo de Pokemón """
+    
+    debilidades = """ 
+    DEBILIDADES Y FORTALEZAS DE CADA TIPO:
+    
+    FUEGO: Debíl contra el Agua <--> Fuerte contra el Planta
+    AGUA: Debíl contra el Eléctrico/Planta <--> Fuerte contra el Fuego
+    PLANTA: Debíl contra el Fuego/Veneno <--> Fuerte contra el Agua
+    ELECTRICO: Debíl contra el Tierra <--> Fuerte contra el Agua
+    """
+    
+    print(debilidades)
